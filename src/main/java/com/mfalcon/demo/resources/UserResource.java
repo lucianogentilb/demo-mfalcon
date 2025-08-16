fcbde1b5bd380af245e75406f3ec9b0779cc3e46
Batistalb <lb.gentil@gmail.com>
2025-08-16 18:52:46 -0400

    User entity and resource

---
+++ src/main/java/com/mfalcon/demo/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.mfalcon.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mfalcon.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Luciano", "luciano@gmail.com", "999999999", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
