f74f3d358791fd3fcf98d3e003ae3acf8a6510a0
Batistalb <lb.gentil@gmail.com>
2025-08-16 19:15:31 -0400

    H2 database, test profile, JPA

--- src/main/java/com/mfalcon/demo/entities/User.java
+++ src/main/java/com/mfalcon/demo/entities/User.java
@@ -3,9 +3,19 @@ package com.mfalcon.demo.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
 	private String name;
 	private String email;
@@ -79,7 +89,5 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(Id, other.Id);
-	}
-
-	
+	}	
 }
