3fd228b494b648d2284d5af87151f719eff19c58
Batistalb <lb.gentil@gmail.com>
2025-08-17 10:13:34 -0400

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/mfalcon/demo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mfalcon.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mfalcon.demo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
